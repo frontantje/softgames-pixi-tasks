0ce4b624b966e16575a3acb785605e6fda334e04
Antje Lemberg <frontantje@gmail.com>
2026-01-28 11:40:55 +0100

    add screen mask

--- src/core/BaseTaskScene.ts
+++ src/core/BaseTaskScene.ts
@@ -1,16 +1,22 @@
 import { SceneManager } from "./SceneManager";
-import { Container } from "pixi.js";
+import { Container, Graphics } from "pixi.js";
 
 export abstract class BaseTaskScene extends Container {
   protected sceneManager: SceneManager;
   protected content: Container;
   private screenWidth: number = 0;
   private screenHeight: number = 0;
+  private screenMask: Graphics;
 
   constructor(sceneManager: SceneManager, name: string) {
     super();
     this.label = name;
     this.sceneManager = sceneManager;
+
+    // Create mask to constrain content to screen bounds
+    this.screenMask = new Graphics();
+    this.addChild(this.screenMask);
+    this.mask = this.screenMask;
 
     // Container for task-specific content
     this.content = new Container();
@@ -20,6 +26,11 @@ export abstract class BaseTaskScene extends Container {
   public onResize(width: number, height: number): void {
     this.screenWidth = width;
     this.screenHeight = height;
+
+    // Update mask to match screen size
+    this.screenMask.clear();
+    this.screenMask.rect(0, 0, width, height);
+    this.screenMask.fill(0xffffff);
 
     // Let child class handle content positioning
     this.onContentResize(width, height);
